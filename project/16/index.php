97177a560169e827bf19c94495ac1f074cdf0fa2
Robert <gbobo1997@yahoo.com>
2018-10-06 13:11:30 -0600

    16.4 - CRUD

--- project/16/index.php
+++ project/16/index.php
@@ -1,6 +1,21 @@
 <?php require_once 'views.php';
+require_once 'db.php';
 $site_title = 'BACS 350 Assignments';
 $page_title = 'MVC Pattern - Project 16';
 begin_page($site_title, $page_title);
 // enter extra content here
+// CRUD insert
+echo '<form action="insert.php" method="post">
+        <label>What is your name?</label>
+        <input type="text" name="my_name"><br>
+        <label>What is your email?</label>
+        <input type="text" name="my_email"><br>
+        <input type="submit" value="Save"/>
+    </form>';
+// CRUD select/list
+$db = remote_connect();
+render_list(query_subscribers($db));
+
+
+end_page();
 ?>
