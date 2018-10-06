3a79fada8c2e3cc166376af62d783da02b520863
Robert Carver <gbobo1997@yahoo.com>
2018-10-06 15:06:27 -0600

    Bug Fixing for 16

--- project/16/index.php
+++ project/16/index.php
@@ -1,5 +1,6 @@
-<?php require_once 'views.php';
+<?php require_once 'subscriber_functions.php';
 require_once 'db.php';
+require_once 'views.php';
 $site_title = 'BACS 350 Assignments';
 $page_title = 'MVC Pattern - Project 16';
 begin_page($site_title, $page_title);
@@ -10,7 +11,7 @@ echo '<form action="insert.php" method="post">
         <input type="text" name="my_name"><br>
         <label>What is your email?</label>
         <input type="text" name="my_email"><br>
-        <input type="submit" value="Save"/>
+        <input type="submit" value="Enter"/>
     </form>';
 // CRUD select/list
 $db = remote_connect();
--- project/16/insert.php
+++ project/16/insert.php
@@ -1,16 +1,13 @@
 <?php
+     // Pick out the inputs
+    $name = filter_input(INPUT_POST, 'my_name');
+    $email = filter_input(INPUT_POST, 'my_email', FILTER_VALIDATE_EMAIL);
+    $success ='index.php';
 
     // Connect to the database
     require_once 'db.php';
-    $db = remote_connect();
-
-
-    // Pick out the inputs
-    $name = filter_input(INPUT_GET, 'name');
-    $email = filter_input(INPUT_GET, 'email');
-
+    $db = remote_connect();   
 
     // Add record
     add_subscriber ($db, $name, $email, 'index.php');
-
 ?>
