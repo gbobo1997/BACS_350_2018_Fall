977e6cf45a365a66b5d2875ee4a764755f8c2c62
Robert <gbobo1997@yahoo.com>
2018-10-06 12:47:48 -0600

    16.1-16.2 - Create app page

---
+++ project/16/index.php
@@ -0,0 +1,6 @@
+<?php require_once 'views.php';
+$site_title = 'BACS 350 Assignments';
+$page_title = 'MVC Pattern - Project 16';
+begin_page($site_title, $page_title);
+// enter extra content here
+?>
