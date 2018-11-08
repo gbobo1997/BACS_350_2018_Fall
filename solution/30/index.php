e3c094d6f29ddf385850fbc87dec5af7d6ddd9db
Mark Seaman <Mark.Seaman@imac.net>
2018-11-08 09:33:25 -0700

    Add login and signup buttons

--- solution/30/index.php
+++ solution/30/index.php
@@ -13,19 +13,33 @@
     $content = render_button('Templates', '../../templates');
     $content .= render_button('Solutions', '..');
     $content .= render_button('Show Log', 'pagelog.php');
+    $content .= render_button('Login', 'index.php?action=login');
+    $content .= render_button('Sign Up', 'index.php?action=signup');
 
     // Try this login
 
-//    $email = "me@here.com";
-//    $password = 'Rock on dude!';
-//
-//    // User Setup:
-//    // require_once 'db.php';
-//    // register_user($db, $email, $password, 'New', 'User');
-//        
-//    $content .= $auth->show_valid ($email, $password);
+    //    $email = "me@here.com";
+    //    $password = 'Rock on dude!';
+    //
+    //    // User Setup:
+    //    // require_once 'db.php';
+    //    // register_user($db, $email, $password, 'New', 'User');
+    //        
+    //    $content .= $auth->show_valid ($email, $password);
 
-    $content = sign_up_form() . login_form();
+
+    function handle_actions() {
+        $action = filter_input(INPUT_GET, 'action');
+        if ($action == 'signup') {
+            $this->log("Show Sign Up Form");
+            $content .= sign_up_form();
+        }
+        if ($action == 'login') {
+            $this->log("Show Sign Up Form");
+            $content .= login_form();
+        }
+    }
+
 
     // Create main part of page content
     $settings = array(
