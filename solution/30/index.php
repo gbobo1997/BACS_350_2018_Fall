67f6bc7adb831a72c3dd29d593431becff5abe14
Mark Seaman <Mark.Seaman@imac.net>
2018-11-08 09:14:20 -0700

    Sign up and login forms

--- solution/30/index.php
+++ solution/30/index.php
@@ -16,15 +16,16 @@
 
     // Try this login
 
-    $email = "me@here.com";
-    $password = 'Rock on dude!';
+//    $email = "me@here.com";
+//    $password = 'Rock on dude!';
+//
+//    // User Setup:
+//    // require_once 'db.php';
+//    // register_user($db, $email, $password, 'New', 'User');
+//        
+//    $content .= $auth->show_valid ($email, $password);
 
-    // User Setup:
-    // require_once 'db.php';
-    // register_user($db, $email, $password, 'New', 'User');
-        
-    $content .= $auth->show_valid ($email, $password);
-
+    $content = sign_up_form() . login_form();
 
     // Create main part of page content
     $settings = array(
